a6a763cb64aa4fab2e5e42e7bc4f8cacae9211c0
RtKuvshin <retunskijmihail071@gmail.com>
2024-09-10 17:12:18 +0300

    Singletone pattern, json save

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,8 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
 }
