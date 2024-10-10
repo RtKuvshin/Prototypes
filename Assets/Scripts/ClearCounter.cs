aceabb08d94557aaa449c3b66c29fad7ac6677e7
RtKuvshin <retunskijmihail071@gmail.com>
2024-10-10 17:50:53 +0300

    KitchenObject Parent

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,16 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO _kitchenObjectSo;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondCounter;
+    [SerializeField] private bool isTesting;
+    
+    private KitchenObject _kitchenObject;
     public void Interact()
     {
-        Transform kitchenObjTransform = Instantiate(_kitchenObjectSo.prefab, counterTopPoint);
-        kitchenObjTransform.localPosition = Vector3.zero;
-        
-        Debug.Log(kitchenObjTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objName);
+        if (_kitchenObject == null)
+        {
+            Transform kitchenObjTransform = Instantiate(_kitchenObjectSo.prefab, counterTopPoint);
+            kitchenObjTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(_kitchenObject.GetClearCounter());
+        }
+    }
+
+    private void Update()
+    {
+        if (isTesting && Input.GetKeyDown(KeyCode.F))
+        {
+            if(_kitchenObject != null)
+            {
+              _kitchenObject.SetClearCounter(secondCounter);  
+            }
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        _kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return _kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        _kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return _kitchenObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
+    
+    private ClearCounter _clearCounter;
 
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSo;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if(_clearCounter != null) _clearCounter.ClearKitchenObject();
+        
+        _clearCounter = clearCounter;
+        if( clearCounter.HasKitchenObject()) Debug.LogError("counter already has a kitchen object");
+        _clearCounter.SetKitchenObject(this);
+        
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return _clearCounter;
     }
 }
