ec40fa14c922433673719376d6eba5e1bca2a87b
RtKuvshin <retunskijmihail071@gmail.com>
2024-10-15 16:28:36 +0300

    Player PickUp

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,38 +4,26 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
+public class ClearCounter : MonoBehaviour, IKitchenObjectParent
 {
     [SerializeField] private KitchenObjectSO _kitchenObjectSo;
     [SerializeField] private Transform counterTopPoint;
-    [SerializeField] private ClearCounter secondCounter;
-    [SerializeField] private bool isTesting;
+
     
     private KitchenObject _kitchenObject;
-    public void Interact()
+    public void Interact(Player player)
     {
         if (_kitchenObject == null)
         {
             Transform kitchenObjTransform = Instantiate(_kitchenObjectSo.prefab, counterTopPoint);
-            kitchenObjTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+            kitchenObjTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
         }
         else
         {
-            Debug.Log(_kitchenObject.GetClearCounter());
+          _kitchenObject.SetKitchenObjectParent(player);
         }
     }
-
-    private void Update()
-    {
-        if (isTesting && Input.GetKeyDown(KeyCode.F))
-        {
-            if(_kitchenObject != null)
-            {
-              _kitchenObject.SetClearCounter(secondCounter);  
-            }
-        }
-    }
-
+    
     public Transform GetKitchenObjectFollowTransform()
     {
         return counterTopPoint;
